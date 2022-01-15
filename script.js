94395afa6f617ebfb4009b3c175e3afb0902863a
Tonycrz <tonybaidoo0@gmail.com>
2022-01-15 13:21:05 +0000

    abit more js

--- script.js
+++ script.js
@@ -1,4 +1,47 @@
 $(document).ready(function() {
+    // autoresize input-tweet field
     autosize($('textarea'));
-    // console.log('working..')
+    
+    // Highlight searchbox onclick
+    $('.search-box').click(function () {
+        $(this).addClass('search-box-highlight')
+    })
+
+        // $('html').click(function () {
+        //     if ($('.search-box').hasClass('search-box-hightlight'))
+        //     {
+        //         $('.search-box').removeClass('search-box-hightlight')
+        //     }
+        // })
+
+    // Show or Hide extra trend topics
+    $('.show-more').click(function () {
+        $('.extra-topics').toggleClass('show')
+        $("#text-toggle").fadeOut(function () {
+            $("#text-toggle").text(($("#text-toggle").text() == 'Show more trends') ? 'Hide extra trends' : 'Show more trends').fadeIn()
+        })
+    })
+
+    // Highlights selected Nav menu-option
+    const nodes = document.querySelectorAll('.color-toggle');
+    nodes.forEach((node) => {
+    node.addEventListener('click', (e) => {
+    nodes.forEach(ele => {
+      if(ele === e.target){
+        ele.style.fontWeight = '700'
+        ele.style.boxShadow= '0 0 0 0.5pt #1b97e9'
+      } else {
+        ele.style.fontWeight = '300'
+        ele.style.boxShadow= 'none'
+      }
+    })
+  })
+})
+
+
+    
+
+
+
+
 })
